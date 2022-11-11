6b9ddb83b55ef794cb51b366c777c9dfe08218c9
sayal00 <sayal.chitrakar00@gmail.com>
2022-11-12 00:36:04 +0545

    added cart and check out routes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+
 const itemRoutes = require('./Routes/itemRoutes');
 const userRoutes = require('./Routes/userRoutes');
+const cartRoutes = require('./Routes/cartRoutes');
+const checkOutRoutes = require('./Routes/checkOutRoutes');
 
 app.use(express.json());
 
 app.use('/api/v1/item',itemRoutes);
 app.use('/api/v1/user',userRoutes);
-
+app.use('/api/v1/cart',cartRoutes);
+app.use('/api/v1/checkout',checkOutRoutes);
 module.exports = app;
